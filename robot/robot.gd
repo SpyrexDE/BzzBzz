adf4aae80f6f3a3b444a6345dc71a2964bfb38ec
Max Schmidt <to.maxschmidt@pm.me>
2024-06-07 21:55:02 +0200

    add basic movement

--- robot/robot.gd
+++ robot/robot.gd
@@ -1,6 +1,15 @@
 extends CharacterBody2D
 
 @export var speed: float
+@export var friction: float
+@export var steering_speed: float
+var _velocity: Vector2
 
-func _physics_process(delta):
+func _physics_process(_delta):
+	var steering = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
+
+	rotation += steering_speed * steering
+
+	velocity = Vector2(0, -speed * Input.get_action_strength("go_forward")).rotated(rotation)
+
 	move_and_slide()
