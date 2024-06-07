ffae610d0f5619ac04e00049e43d480c7e3059cc
SimonKeck <simonkeck64@gmail.com>
2024-06-07 22:19:55 +0200

    add haus and robot to testlvl2

--- props/haus.gd
+++ props/haus.gd
@@ -8,4 +8,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	
 	pass
+
+
+
+func _on_area_2d_body_entered(body):
+	print("hi")
+	pass # Replace with function body.
