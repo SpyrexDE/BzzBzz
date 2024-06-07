fb6913f4efd077eec031734cfbe7a4af55f391ca
Spyrex <malikfk100@gmail.com>
2024-06-07 22:36:24 +0200

    Refine player movement

--- robot/robot.gd
+++ robot/robot.gd
@@ -1,15 +1,16 @@
 extends CharacterBody2D
+class_name Player
 
 @export var speed: float
 @export var friction: float
 @export var steering_speed: float
-var _velocity: Vector2
 
 func _physics_process(_delta):
 	var steering = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
 
 	rotation += steering_speed * steering
 
-	velocity = Vector2(0, -speed * Input.get_action_strength("go_forward")).rotated(rotation)
+	velocity += Vector2(0, -speed * Input.get_action_strength("go_forward")).rotated(rotation)
+	velocity = velocity / friction
 
 	move_and_slide()
