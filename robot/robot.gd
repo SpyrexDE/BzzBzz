4f03ff3c4a0a680c64acc5b731718aa40ada00dc
Spyrex <malikfk100@gmail.com>
2024-06-08 13:29:59 +0200

    Add friction_multiplier to player

--- robot/robot.gd
+++ robot/robot.gd
@@ -5,12 +5,17 @@ class_name Player
 @export var friction: float
 @export var steering_speed: float
 
+var friction_multiplier: float = 1.0
+
 func _physics_process(_delta):
 	var steering = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
 
-	rotation += steering_speed * steering
+	rotation += steering_speed * steering / friction_multiplier
 
-	velocity += Vector2(0, -speed * Input.get_action_strength("go_forward")).rotated(rotation)
-	velocity = velocity / friction
+	var input = Input.get_action_strength("go_forward") - Input.get_action_strength("go_backward")
+	velocity += Vector2(0, -speed * input).rotated(rotation)
+	
+	var total_friction = friction * friction_multiplier
+	velocity = velocity / total_friction
 
 	move_and_slide()
