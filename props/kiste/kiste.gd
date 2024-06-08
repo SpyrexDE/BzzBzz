963827c90fa50792b1cc1ac4d6730dcb04a7b546
Max Schmidt <to.maxschmidt@pm.me>
2024-06-08 18:38:51 +0200

    add game over by kiste

--- props/kiste/kiste.gd
+++ props/kiste/kiste.gd
@@ -1,11 +1,6 @@
 extends RigidBody2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_area_entered(area: Area2D):
+	var zap = area.get_parent()
+	if zap is Zap:
+		zap.kill()
