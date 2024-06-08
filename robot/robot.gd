149023e85794ef3a19747f24a59ce2e6073273ea
Spyrex <malikfk100@gmail.com>
2024-06-08 14:36:46 +0200

    Add drift to robot

--- robot/robot.gd
+++ robot/robot.gd
@@ -9,13 +9,15 @@ var friction_multiplier: float = 1.0
 
 func _physics_process(_delta):
 	var steering = Input.get_action_strength("steer_right") - Input.get_action_strength("steer_left")
+	var drift_factor = min(0.1, abs(steering))
+	var drift_vector = Vector2(drift_factor, drift_factor)
 
 	rotation += steering_speed * steering / friction_multiplier
 
 	var input = Input.get_action_strength("go_forward") - Input.get_action_strength("go_backward")
-	velocity += Vector2(0, -speed * input).rotated(rotation)
+	velocity += Vector2(0, -speed * input - drift_factor * 500).rotated(rotation)
 	
-	var total_friction = friction * friction_multiplier
+	var total_friction = friction * friction_multiplier - drift_factor
 	velocity = velocity / total_friction
 
 	move_and_slide()
