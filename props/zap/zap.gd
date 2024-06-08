655a24ef3456c7fbc86abf61a43c2446a0725edc
Max Schmidt <to.maxschmidt@pm.me>
2024-06-08 17:13:57 +0200

    update zap

--- props/zap/zap.gd
+++ props/zap/zap.gd
@@ -5,6 +5,7 @@ var connector: Connector
 var checkpoint_n: int
 
 var dist_from_last: float
+var last_checkpoint: Vector2
 
 func _ready():
 	connector = %Connector
@@ -12,18 +13,25 @@ func _ready():
 	checkpoint_n = 0
 	position = connector.checkpoints[checkpoint_n]
 	checkpoint_n += 1
+	last_checkpoint = connector.checkpoints[0]
 
 func _physics_process(_delta):
+	dist_from_last += speed
+
+	#is at checkpoint
 	if len(connector.checkpoints) > checkpoint_n:
-		if (position - connector.checkpoints[checkpoint_n]).length() < 5:
-				checkpoint_n += 1
+		if (position - connector.checkpoints[checkpoint_n]).length() < 10:
+			last_checkpoint = connector.checkpoints[checkpoint_n]
+			checkpoint_n += 1
+			dist_from_last = 0
+	
+	#move along cable
 	if len(connector.checkpoints) > checkpoint_n:
-		dist_from_last = 0
-		position += speed * (-position + connector.checkpoints[checkpoint_n]).normalized()
+		var dir = (connector.checkpoints[checkpoint_n] - last_checkpoint).normalized()
+		position = connector.checkpoints[checkpoint_n] + dir * dist_from_last
 	else:
-		if (position - connector.robot.position).length() > 5:
+		if (position - connector.robot.position).length() > 10:
 			var dir = (connector.robot.position - connector.checkpoints[- 1]).normalized()
-			dist_from_last += speed;
 			position = connector.checkpoints[- 1] + dir * dist_from_last
 		else:
 			print("dead")
