b95b968e6149696cbdd3e7db40065567a7fa1730
Max Schmidt <to.maxschmidt@pm.me>
2024-06-09 00:24:41 +0200

    add sound player

--- ui/main_menu.gd
+++ ui/main_menu.gd
@@ -1,7 +1,12 @@
 extends Control
 
+@export var menusoun: AudioStreamMP3
+
+func _ready():
+	SoundEffects.play_sound(menusoun, 1, 0, "bgm")
 
 func _on_start_button_pressed() -> void:
+	SoundEffects.fade_sound(menusoun)
 	get_tree().change_scene_to_file("res://ui/rules_menu.tscn")
 
 func _on_start_button_2_pressed() -> void:
