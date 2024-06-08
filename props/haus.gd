d6afd3a3ecd667d0d60b31243e9ac1c9d00dff38
Max Schmidt <to.maxschmidt@pm.me>
2024-06-08 14:33:56 +0200

    fix intersection

--- props/haus.gd
+++ props/haus.gd
@@ -6,20 +6,16 @@ var strom := false
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	_lightup()
 	pass
-
-
 
 func _on_area_2d_body_entered(body):
 	if body is Player:
 		print("Setze das Kabel auf das Haus")
 		$AnimationPlayer.play("haus_light_up")
 	pass # Replace with function body.
-	
 	
 func _lightup():
 	if strom == true:
