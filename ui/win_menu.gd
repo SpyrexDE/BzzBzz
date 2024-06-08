53b11d57a5246f6ba27e0f1e4ee57be892ae5ae5
Spyrex <malikfk100@gmail.com>
2024-06-08 21:43:10 +0200

    Add score label to win menu

--- ui/win_menu.gd
+++ ui/win_menu.gd
@@ -2,7 +2,11 @@ extends Control
 
 
 @export var next_level: PackedScene
-
+@export var score: int:
+	set(val):
+		score = val
+		$Label.show()
+		$Label.text = "Score: " + str(val)
 
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file(next_level.resource_path)
