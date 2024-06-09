192b46a3344cd895029e47192c05f5289930b559
Max Schmidt <to.maxschmidt@pm.me>
2024-06-09 11:07:35 +0200

    audio is done, so am i

--- ui/game_over_menu.gd
+++ ui/game_over_menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
 @export var meow: AudioStreamMP3
+@export var pitch: float
 
 func _ready():
-	SoundEffects.play_sound(meow)
+	SoundEffects.play_sound(meow, pitch)
 
 func _on_back_button_pressed() -> void:
 	
